69b5e850521a795c6a30994bbcbb29b4a173c8db
mychas2115 <82595103+mychas2115@users.noreply.github.com>
2022-02-26 13:02:34 +0100

    Add files via upload

---
+++ zagadki.js
@@ -0,0 +1,100 @@
+function sprawdz()
+    {
+        document.getElementById('animate__show').innerHTML= '<span style="font-size:20px"> Kasia (Jeśli mama Kasi ma pięć córek, a ich imiona wymieniono wyżej, to znaczy, że piątą córką jest właśnie Kasia.). </span>'; 
+        document.getElementById('animate__show').style.color='blueviolet';
+        document.getElementById('animate__show').style.fontFamily= 'bellota';
+        document.getElementById('animate__show').style.animationName= 'jackInTheBox';
+        document.getElementById('animate__show').style.animationTimingFunction= 'ease-in-out'
+        document.getElementById('animate__show').style.textAlign= 'center';
+    }
+            function ukryj()
+            {
+                document.getElementById("animate__show").innerHTML = '<span style="font-size:20px"> Mama Kasi ma pięć córek. Jeśli imiona jej córek to odpowiednio Klara, Karolina, Klaudia, Kinga, to jakie będzie imię piątej córki?. </span>';
+                document.getElementById('animate__show').style.color='blueviolet';
+                document.getElementById('animate__show').style.fontFamily= 'bellota';
+                document.getElementById('animate__show').style.animationName= 'jackInTheBox';
+                document.getElementById('animate__show').style.animationTimingFunction= 'ease-in-out'
+                document.getElementById('animate__show').style.textAlign= 'center';
+            }   
+
+
+            
+function sprawdz2()
+    {
+                document.getElementById('animate__show2').innerHTML= '<span style="font-size:20px"> 22 (W skrajnym przypadku najpierw wyciągniemy wszystkie kule zielone i niebieskie, a na końcu 2 żółte, w sumie 22). </span>'; 
+                document.getElementById('animate__show2').style.color='blueviolet';
+                document.getElementById('animate__show2').style.fontFamily= 'bellota';
+                document.getElementById('animate__show2').style.animationName= 'jackInTheBox';
+                document.getElementById('animate__show2').style.animationTimingFunction= 'ease-in-out'
+                document.getElementById('animate__show2').style.textAlign= 'center';
+     }
+            function ukryj2()
+            {
+                document.getElementById("animate__show2").innerHTML = '<span style="font-size:17px"> W pudełku mamy po 10 kul: żółtych, zielonych, niebieskich. Sięgamy po nie losowo. Ile kul trzeba wyciągnąć, by mieć pewność, że trafiło się na 2 żółte kule? </span>';
+                document.getElementById('animate__show2').style.color='blueviolet';
+                document.getElementById('animate__show2').style.fontFamily= 'bellota';
+                document.getElementById('animate__show2').style.animationName= 'jackInTheBox';
+                document.getElementById('animate__show2').style.animationTimingFunction= 'ease-in-out'
+                document.getElementById('animate__show2').style.textAlign= 'center';
+            }   
+
+function sprawdz3()
+    {
+        document.getElementById("animate__show3").innerHTML= '<span style="font-size:20px"> Mył okna na pierwszym piętrze i stamtąd spadł. </span>'; 
+        document.getElementById('animate__show3').style.color='blueviolet';
+        document.getElementById('animate__show3').style.fontFamily= 'bellota';
+        document.getElementById('animate__show3').style.animationName= 'jackInTheBox';
+        document.getElementById('animate__show3').style.animationTimingFunction= 'ease-in-out'
+        document.getElementById('animate__show3').style.textAlign= 'center';
+    }
+
+            function ukryj3()
+            {
+                document.getElementById("animate__show3").innerHTML = '<span style="font-size:20px"> Człowiek mył okna w 20-piętrowym budynku. W pewnym momencie stracił równowagę i spadł, ale nie został ranny. Jak to możliwe?</span>';
+                document.getElementById('animate__show3').style.color='blueviolet';
+                document.getElementById('animate__show3').style.fontFamily= 'bellota';
+                document.getElementById('animate__show3').style.animationName= 'jackInTheBox';
+                document.getElementById('animate__show3').style.animationTimingFunction= 'ease-in-out'
+                document.getElementById('animate__show3').style.textAlign= 'center';
+            }   
+
+function sprawdz4()
+    {
+        document.getElementById("animate__show4").innerHTML= '<span style="font-size:25px"> Mężczyzna, który prosił o wodę, miał czkawkę i kelner postanowił, że go przestraszy, żeby mu pomóc. </span>'; 
+        document.getElementById('animate__show4').style.color='blueviolet';
+        document.getElementById('animate__show4').style.fontFamily= 'bellota';
+        document.getElementById('animate__show4').style.animationName= 'jackInTheBox';
+        document.getElementById('animate__show4').style.animationTimingFunction= 'ease-in-out'
+        document.getElementById('animate__show4').style.textAlign= 'center';
+    }
+            function ukryj4()
+            {
+                document.getElementById("animate__show4").innerHTML = '<span style="font-size:16px"> Jesteśmy w barze. Wchodzi mężczyzna, podchodzi do baru i prosi kelnera o szklankę wody. Kelner patrzy na niego przez kilka sekund, po czym sięga pod ladę i wyciąga broń. Kieruje ją prosto w twarz mężczyzny, który, przerywanym głosem, prosił wcześniej o wodę. Mężczyzna widząc, że kelner mierzy w niego bronią, wzdycha głęboko, dziękuje i odchodzi. Co tam się takiego stało? </span>';
+                document.getElementById('animate__show4').style.color='blueviolet';
+                document.getElementById('animate__show4').style.fontFamily= 'bellota';
+                document.getElementById('animate__show4').style.animationName= 'jackInTheBox';
+                document.getElementById('animate__show4').style.animationTimingFunction= 'ease-in-out'
+                document.getElementById('animate__show4').style.textAlign= 'center';
+            }   
+
+function sprawdz5()
+    {
+        document.getElementById('animate__show5').innerHTML= '<span style="font-size:27px"> Mężczyzna był latarnikiem. Przez niego statek rozbił się o skały. Zginęło 90 osób. </span>'; 
+        document.getElementById('animate__show5').style.color='blueviolet';
+        document.getElementById('animate__show5').style.fontFamily= 'bellota';
+        document.getElementById('animate__show5').style.animationName= 'jackInTheBox';
+        document.getElementById('animate__show5').style.animationTimingFunction= 'ease-in-out'
+        document.getElementById('animate__show5').style.textAlign= 'center';
+        document.getElementById('animate__show5').style.marginBottom= '20px';
+    }
+
+            function ukryj5()
+            {
+                document.getElementById('animate__show5').innerHTML = '<span style="font-size:18px"> Teraz poznamy pewnego człowieka. Mieszkał sam, w odludnym miejscu, do którego nikt nigdy nie zaglądał. Pewnego dnia mężczyzna popełnił błąd. Niechcący zgasił bowiem wszystkie światła u siebie w domu. Po wyjściu na zewnątrz, okazało się, że zginęło 90 osób. Zalał się łzami, ponieważ był jedyną osobą odpowiedzialną za tę tragedię.</span>';
+                document.getElementById('animate__show5').style.color='blueviolet';
+                document.getElementById('animate__show5').style.fontFamily= 'bellota';
+                document.getElementById('animate__show5').style.animationName= 'jackInTheBox';
+                document.getElementById('animate__show5').style.animationTimingFunction= 'ease-in-out'
+                document.getElementById('animate__show5').style.textAlign= 'center';
+                document.getElementById('animate__show5').style.marginBottom= '20px';
+            }
